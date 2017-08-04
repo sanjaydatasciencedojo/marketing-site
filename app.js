2f6b0473ed5db8a800341268237a382fec156b10
Clinton Blackburn <clinton.blackburn@gmail.com>
2017-08-03 21:51:52 -0400

    Using Apache combined log format in production

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(logger(isDev ? 'dev' : 'combined'));
 
 
 // Default render context
